f3a0795744b57c603f54babed0c97e59e7bb99a0
DefNeoRoman <d89056411060@yandex.ru>
2019-04-13 12:59:28 +0300

    groovy valid java

---
+++ src/main/java/com/bankrest/additionalTasks/Main.java
@@ -0,0 +1,5 @@
+package com.bankrest.additionalTasks;
+
+public class Main {
+   
+}
--- src/test/java/SelTest.java
+++ src/test/java/SelTest.java
@@ -1,24 +1,9 @@
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SelTest {
 
     @Test
     public void test() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:/chromedriver/chromedriver.exe ");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://dclite.ru/");
-        driver.findElement(By.id("c-header__reg-btn")).click();
-        Thread.sleep(5000);
-        System.out.println(driver.getCurrentUrl());
-        ((ChromeDriver) driver).findElementById("email").sendKeys("pop");
-        Thread.sleep(5000);
-        ((ChromeDriver) driver).findElementByXPath("html/body/header/div[1]/div/a[3]").click();
-        for(String winHandle : driver.getWindowHandles()){
-            System.out.println(winHandle);
-        }
-        System.out.println(driver.getCurrentUrl());
+
     }
 }
